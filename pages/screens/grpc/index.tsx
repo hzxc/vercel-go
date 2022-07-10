74d98e1a25c9a663d1577a7a9ed49d6ace41fdaa
x <x@x.com>
2022-07-10 13:15:08 +0800

    2022-7-10 13:15

--- pages/screens/grpc/index.tsx
+++ pages/screens/grpc/index.tsx
@@ -1,16 +1,16 @@
-import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
-import { PingPongServiceClient } from "gen/node/pingpong/v1/pingpong.client";
-import React from "react";
+import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
+import { PingPongServiceClient } from 'gen/node/pingpong/v1/pingpong.client';
+import type { NextPage } from 'next';
 
-export const GrpcScreen = () => {
+export const GrpcScreen: NextPage = () => {
   const handleReq = () => {
     const client = new PingPongServiceClient(
       new GrpcWebFetchTransport({
-        baseUrl: "http://localhost:3000",
-        format: "text",
+        baseUrl: 'http://localhost:3000',
+        format: 'text',
       })
     );
-    const call = client.pingPong({ ping: "ping" }, {});
+    const call = client.pingPong({ ping: 'ping' }, {});
     call.response.then((value) => {
       console.log(value);
     });
